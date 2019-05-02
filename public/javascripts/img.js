e68ec812bb8fa9121da3749e49e1facf3047be9c
Charlie <chen_610146@hotmail.com>
2019-05-02 21:27:59 +0800

    css edit

--- public/javascripts/img.js
+++ public/javascripts/img.js
@@ -3,15 +3,15 @@ const category = document.querySelectorAll('.category')
 
 category.forEach(item => {
   if (item.textContent == 'home') {
-    item.innerHTML = `<i class="fas fa-home fa-4x m-auto"></i>`
+    item.innerHTML = `<i class="fas fa-home fa-4x m-auto icon"></i>`
   } else if (item.textContent == 'travel') {
-    item.innerHTML = `<i class="fas fa-shuttle-van fa-4x m-auto"></i>`
+    item.innerHTML = `<i class="fas fa-shuttle-van fa-4x m-auto icon"></i>`
   } else if (item.textContent == 'leisure') {
-    item.innerHTML = `<i class="fas fa-grin-beam fa-4x m-auto"></i>`
+    item.innerHTML = `<i class="fas fa-grin-beam fa-4x m-auto icon"></i>`
   } else if (item.textContent == 'food') {
-    item.innerHTML = `<i class="fas fa-utensils fa-4x m-auto"></i>`
+    item.innerHTML = `<i class="fas fa-utensils fa-4x m-auto icon"></i>`
   } else if (item.textContent == 'other') {
-    item.innerHTML = `<i class="fas fa-pen fa-4x m-auto"></i>`
+    item.innerHTML = `<i class="fas fa-pen fa-4x m-auto icon"></i>`
 
   }
 
